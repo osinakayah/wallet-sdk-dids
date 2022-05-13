8e39e1ff64c87af8eb71a85fad877657874bd156
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-13 10:34:25 +0100

    First commi

---
+++ lib/interface/i.did.manager.d.ts
@@ -0,0 +1,10 @@
+export interface DIDResponse {
+    id: string;
+    content: any;
+}
+export interface IDidManager {
+    createDID: (options?: any) => Promise<DIDResponse>;
+    getDIDs: () => Promise<Array<DIDResponse>>;
+    saveDID: (didDocument: any) => Promise<DIDResponse>;
+    getWallet: () => any;
+}
