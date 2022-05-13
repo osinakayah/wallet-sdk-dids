24cf7f421ebc14635a1cdcb5370f7f6596f7c8bc
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-13 10:55:08 +0100

    First commi

--- src/did.methods/did.key.manager.ts
+++ src/did.methods/did.key.manager.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
-import { driver } from '@digitalbazaar/did-method-key';
+
+// import { driver } from '@digitalbazaar/did-method-key';
 import { IDidManager, DIDResponse } from '../interface/i.did.manager';
 
 // import {Wallet} from '@docknetwork/wallet-sdk-core/lib/modules/wallet';
@@ -9,6 +10,7 @@ import {Ed25519VerificationKey2020} from '@digitalbazaar/ed25519-verification-ke
 import {CryptoLD} from 'crypto-ld';
 
 // const {Wallet} = require('@docknetwork/wallet-sdk-core/lib/modules/wallet')
+const { driver } = require('@digitalbazaar/did-method-key');
 export class DidKeyManager  implements IDidManager{
   private static instance: IDidManager;
 
--- types/decs.d.ts
+++ types/decs.d.ts
@@ -1,5 +1,5 @@
 
 // declare module "@docknetwork/wallet-sdk-core/lib/modules/wallet"
-declare module "@digitalbazaar/ed25519-verification-key-2020"
-declare module "crypto-ld"
-declare module "@digitalbazaar/did-method-key"
+// declare module "@digitalbazaar/ed25519-verification-key-2020"
+// declare module "crypto-ld"
+// declare module "@digitalbazaar/did-method-key"
